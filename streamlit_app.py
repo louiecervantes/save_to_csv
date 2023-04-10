4d38d517d1a6fbfc8b5e4be9887ee58049343181
unknown <louiecervantes@wvsu.edu.ph>
2023-04-11 04:14:48 +0800

    save changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,11 +21,14 @@ def app():
     # Create a button to save data to CSV file
     if st.button('Save data to CSV file'):
         # Save the dataframe to a CSV file
-        csv = df.to_csv(filename, index=False)
-        b64 = base64.b64encode(csv.encode()).decode()  # Convert to base64
-        href = f'<a href="data:file/csv;base64,{b64}" download="' + fiename + '">Download CSV file</a>'
-        st.markdown(href, unsafe_allow_html=True)       
-        st.success(f'Data saved to {filename}!')         
+        csv = df.to_csv(index=False)
+        if csv:
+            b64 = base64.b64encode(csv.encode()).decode()  # Convert to base64
+            href = f'<a href="data:file/csv;base64,{b64}" download="data.csv">Download CSV file</a>'
+            st.markdown(href, unsafe_allow_html=True)
+        else:
+            st.write("Error: Unable to generate CSV file.")
+      
              
 # run the app
 if __name__ == "__main__":
